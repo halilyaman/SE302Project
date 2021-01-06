db7837b3bf5458dfdf3e12861f31f5ff68309f5b
halilyaman <halilymn03@gmail.com>
2021-01-06 19:50:14 +0300

    Added empty main frame, .gitignore file and empty CreateSyllabusPage class.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        JFrame frame = new JFrame("Syllabus Application");
+        frame.setSize(1280, 720);
+        // center window
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        System.out.println(dimension.width);
+        frame.setLocation(
+                (dimension.width / 2) - (frame.getWidth() / 2),
+                (dimension.height / 2) - (frame.getHeight() / 2));
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
---
+++ src/pages/CreateSyllabusPage.java
@@ -0,0 +1,7 @@
+package pages;
+
+import javax.swing.*;
+
+public class CreateSyllabusPage extends JPanel {
+
+}
