5f9c494d89d2483606cd91f0c0f80663cc2f9387
cenktu <73163075+cenktu@users.noreply.github.com>
2021-01-09 17:06:22 +0300

    Add files via upload

---
+++ src/pages/WelcomePage.java
@@ -0,0 +1,220 @@
+package project;
+//package pages;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JPopupMenu;
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.BoxLayout;
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+//import pages.CreateSyllabusPage;
+public class WelcomePage extends JPanel{
+
+	private JFrame frame;
+	private JPanel topPanel;
+	private JLabel welcomeTextLabel;
+	private JLabel logoLabel;
+	private JPanel buttonPanel;
+	private JPanel displayPanel;
+	private JButton createSyllabusButton;
+	private JButton fetchSyllabusButton;
+	private JButton listSyllabusButton;
+	private JButton exitButton;
+	//private CreateSyllabusPage createPage;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					WelcomePage window = new WelcomePage();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public WelcomePage() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 1120, 659);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		frame.setResizable(false);
+		
+		//createPage = new CreateSyllabusPage();
+		
+		topPanel = new JPanel();
+		createTopPanel();
+		
+		buttonPanel = new JPanel();
+		createMenuPanel();
+		
+		displayPanel = new JPanel();
+		displayPanel.setLayout(null);
+		createDisplayPanel();
+		
+		
+	}
+	public void createSyllabusPage()
+	{
+		//CreateSyllabusPage createPage= new CreateSyllabusPage();
+		
+		//displayPanel.add(createPage);
+	}
+	
+	public void fetchSyllabusPage()
+	{
+		//FetchSyllabusPage fetchPage= new FetchSyllabusPage();
+	}
+	
+	public void syllabusListPage()
+	{
+		//SyllabusListPage listPage= new SyllabusListPage();
+	}
+	
+	public void exitProgram()
+	{
+		System.exit(0);
+	}
+	public void changeColor(JButton button, Color color)
+	{
+		button.setBackground(color);
+	}
+	
+	public void createTopPanel()
+	{
+		topPanel.setBackground(new Color(50, 80, 130));
+		topPanel.setBounds(0, 0, 1115, 132);
+		frame.getContentPane().add(topPanel);
+		topPanel.setLayout(null);
+		
+		welcomeTextLabel = new JLabel("WELCOME TO SYLLABUS EDITOR APP");
+		welcomeTextLabel.setForeground(new Color(224, 255, 255));
+		welcomeTextLabel.setFont(new Font("Monospaced", Font.BOLD, 35));
+		welcomeTextLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		welcomeTextLabel.setBounds(344, 52, 666, 41);
+		topPanel.add(welcomeTextLabel);
+		
+		logoLabel = new JLabel("New label");
+		logoLabel.setIcon(new ImageIcon(WelcomePage.class.getResource("/Icon/ieuLogo2.png")));
+		logoLabel.setBounds(24, 0, 200, 143);
+		topPanel.add(logoLabel);
+	}
+	
+	public void createMenuPanel()
+	{
+		buttonPanel.setBackground(new Color(205, 133, 63));
+		buttonPanel.setBounds(0, 132, 239, 496);
+		frame.getContentPane().add(buttonPanel);
+		buttonPanel.setLayout(null);
+		
+		createSyllabusButton = new JButton("CREATE \r\n\r\r\nSYLLABUS");
+		createSyllabusButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				changeColor(createSyllabusButton,new Color(128, 128, 128));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				changeColor(createSyllabusButton,new Color(211, 211, 211));
+			}
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+				createSyllabusPage();
+				
+			}
+		});
+		createSyllabusButton.setFont(new Font("Palatino Linotype", Font.BOLD, 14));
+		createSyllabusButton.setBackground(new Color(211, 211, 211));
+		createSyllabusButton.setBounds(31, 60, 171, 48);
+		buttonPanel.add(createSyllabusButton);
+		
+		fetchSyllabusButton = new JButton("IMPORT SYLLABUS");
+		fetchSyllabusButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				changeColor(fetchSyllabusButton,new Color(128, 128, 128));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				changeColor(fetchSyllabusButton,new Color(211, 211, 211));
+			}
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				fetchSyllabusPage();
+			}
+		});
+		fetchSyllabusButton.setFont(new Font("Palatino Linotype", Font.BOLD, 14));
+		fetchSyllabusButton.setBackground(new Color(211, 211, 211));
+		fetchSyllabusButton.setBounds(31, 168, 171, 48);
+		buttonPanel.add(fetchSyllabusButton);
+		
+		listSyllabusButton = new JButton("LIST ALL SYLLABI");
+		listSyllabusButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				changeColor(listSyllabusButton,new Color(128, 128, 128));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				changeColor(listSyllabusButton,new Color(211, 211, 211));
+			}
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				syllabusListPage();
+				
+			}
+		});
+		listSyllabusButton.setFont(new Font("Palatino Linotype", Font.BOLD, 15));
+		listSyllabusButton.setBackground(new Color(211, 211, 211));
+		listSyllabusButton.setBounds(31, 276, 171, 48);
+		buttonPanel.add(listSyllabusButton);
+		
+		exitButton = new JButton("EXIT");
+		exitButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				changeColor(exitButton,new Color(128, 128, 128));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				changeColor(exitButton,new Color(211, 211, 211));
+			}
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				exitProgram();
+			}
+		});
+		exitButton.setFont(new Font("Palatino Linotype", Font.BOLD, 17));
+		exitButton.setBackground(new Color(211, 211, 211));
+		exitButton.setBounds(31, 384, 171, 48);
+		buttonPanel.add(exitButton);
+		
+
+	}
+	public void createDisplayPanel()
+	{
+		displayPanel.setBounds(238, 132, 875, 496);
+		frame.getContentPane().add(displayPanel);
+	}
+}
